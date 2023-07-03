f06457ce8fb39993cc12a786d3ec41b4ab070ef4
andriy-h80 <andrey.hanzel@gmail.com>
2023-07-03 16:06:13 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const ctrl = require("../../controllers/contacts");
 
-const {validateBody} = require("../../midleewares");
+const {validateBody} = require("../../middlewares");
 
 const schemas = require("../../schemas/contacts");
 
